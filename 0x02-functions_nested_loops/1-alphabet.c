16bb47536b17c0e2286eb62fa2ee96b1d10e0036
Qudus Musiliu <58162408+Adeem-Treef@users.noreply.github.com>
2022-06-08 10:01:29 +0100

    print numbers

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ * Return: List of alphabeth from a - z
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
